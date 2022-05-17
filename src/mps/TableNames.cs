89bbfe4cef1e45f3fcbb07c7f01f0926e59887ba
Mohammad Rahhal <m.r992@hotmail.com>
2022-05-17 19:46:21 +0200

    Create TableNames.cs

---
+++ src/mps/TableNames.cs
@@ -0,0 +1,9 @@
+﻿namespace mps;
+
+public static class TableNames
+{
+	public static readonly string Folder = nameof(Folder);
+	public static readonly string Item = nameof(Item);
+	public static readonly string Album = nameof(Album);
+	public static readonly string AlbumItemLink = nameof(AlbumItemLink);
+}
